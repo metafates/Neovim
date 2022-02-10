e8a1375b1d61318e57adbb9dbe959b0309c161d5
Lain <vvz.dev@gmail.com>
2022-02-10 19:43:15 +0300

    packer bootstrap added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,11 @@
+-- Automatically install and set up packer.nvim on any machine this config is cloned to
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'lifepillar/vim-gruvbox8'
